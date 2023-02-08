d381db8ebee8d6af8defa56c631f3658cedb1d99
Vishnu Vardhan <122501930+9014244373@users.noreply.github.com>
2023-02-08 10:50:33 +0530

    Create cycle 60.c

---
+++ cycle 60.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() { 
+
+
+int n,square,cube;
+printf("enter the number:");
+scanf("%d",&n);
+square=n*n;
+cube=n*n*n;
+printf("the square is:%d\n",square);
+printf("the cube is:%d\n",cube);
+return 0;
+}
